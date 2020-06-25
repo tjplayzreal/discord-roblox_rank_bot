a7a102233a2dc5281c505272d4ec8d9b88fa51b7
tjplayzreal <tjplayzroblox@gmail.com>
2020-06-24 22:31:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+// just a configured version of yogurtsyums' qbot
 
+const express = require('express')
+const app = express()
+
+app.get('/', (request, response) => {
+  response.sendStatus(200)
+})
+
+let listener = app.listen(process.env.PORT, () => {
+  console.log('Your app is listening on port:' + listener.address().port)
+})
+
+// lol im dumb ima pick this up in the morining
