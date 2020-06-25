4ac9fe088a00b00ac0e62eccdf1ed0601595936c
tjplayzreal <tjplayzroblox@gmail.com>
2020-06-24 22:40:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,4 +11,21 @@ let listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port:' + listener.address().port)
 })
 
-// lol im dumb ima pick this up in the morining
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const roblox = require('noblox.js')
+const chalk = require('chalk')
+const figlet = require('figlet')
+require('dotenv').config()
+const fs = require('fs')
+
+roblox.setCookie(process.env.cookie).catch(async err => {
+  console.log(chalk.red('Issue with logging in: ' + err))
+})
+
+let commandlist = []
+
+let OneShout = true
+let shout
+
+// picking this up later
